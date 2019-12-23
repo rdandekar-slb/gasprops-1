efea2c3fa783260844eae6958fec7bb55931a0d2
Rashmin Dandekar <rdandekar@slb.com>
2019-12-23 14:20:05 +0800

    cleaned up few things

--- main.rb
+++ main.rb
@@ -1,28 +1,18 @@
 require 'sinatra'
-require 'yaml/store'
 require './gas.rb'
 
 set :protection, :except => :frame_options
 set :bind, '0.0.0.0'
 
-# Choices = {
-#   'HAM' => 'Hamburger',
-#   'PIZ' => 'Pizza',
-#   'CUR' => 'Curry',
-#   'NOO' => 'Noodles',
-# }
+
 
 get '/' do
-  # @title="Gas Properties Calculator"
- 
+
   erb :index
 end
 
 
 post '/calc' do
-  # params.each do |k,v|
-  #   p k,v
-  # end
   
   @press=params["press"].to_f
   @temp=params["temp"].to_f
@@ -33,8 +23,5 @@ end
 
 
 get '/results' do
-  # @title = 'Results so far:'
-  # @store = YAML::Store.new 'votes.yml'
-  # @votes = @store.transaction { @store['votes'] }
-  # erb :results
+
 end
